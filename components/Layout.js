11ff084211ee6c3ddcd4c266bbf0771693ed4dbc
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-05 22:17:13 +0100

    mb-auto p-4

--- components/Layout.js
+++ components/Layout.js
@@ -4,9 +4,9 @@ import Navbar from './Navbar'
 const Layout = ({ children }) => {
   return (
     <>
-      <div className="bg-purple-10">
+      <div className="bg-purple-200 flex flex-col min-h-screen justify-between">
         <Navbar />
-        <div className="flex">
+        <div className="flex flex-col mb-auto p-4">
           <main>{children}</main>
         </div>
         <Footer />
