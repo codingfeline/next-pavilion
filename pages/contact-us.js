e28e4b32d03ab214e0e130df1890c46b77e90334
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-05 22:55:11 +0100

    form elements

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -4,6 +4,26 @@ const ContactUs = () => {
   return (
     <>
       <div>ContactUs</div>
+      <form>
+        <div>
+          <label htmlFor="name">
+            Name
+            <input type="text" id="name" />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="email">
+            Email
+            <input type="text" id="email" />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="message">
+            Message
+            <textarea id="message" cols="30" rows="10"></textarea>
+          </label>
+        </div>
+      </form>
     </>
   )
 }
