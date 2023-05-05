943e88684e6be47c2182e8233d3a6b56bdb5cebf
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-05 22:16:42 +0100

    copy symbol

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="bg-slate-100 py-2 px-4">{`@ ${new Date().getFullYear()}`}</footer>
+    <footer className="bg-slate-100 py-2 px-4">
+      &copy;{` ${new Date().getFullYear()}`}
+    </footer>
   )
 }
 
