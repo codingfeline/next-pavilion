7f46677b19ba16b6c891d597e9921c38dbe61761
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-06 16:42:50 +0100

    fetched menus using getStaticProps

--- pages/main-menu.js
+++ pages/main-menu.js
@@ -1,11 +1,25 @@
+import Table4col_1_price from '@/components/table_4col_1_price'
 import React from 'react'
 
-const MainMenu = () => {
+const MainMenu = ({ menus }) => {
   return (
     <>
-      <div>MainMenu</div>
+      <div className="bg-slate-100">
+        <Table4col_1_price menus={menus} title={'Main Menus'} />
+      </div>
     </>
   )
 }
 
 export default MainMenu
+
+export async function getStaticProps() {
+  const res = await fetch(
+    'https://bangkokpavilion.co.uk/api/all_menus?menu=main'
+  )
+  const menus = await res.json()
+
+  return {
+    props: { menus },
+  }
+}
