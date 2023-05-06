6788b4509992f62c2ac838b587c4c94b70061e2b
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-06 23:39:45 +0100

    rendering recurring table components using array

--- pages/main-menu.js
+++ pages/main-menu.js
@@ -1,11 +1,30 @@
 import Table4col_1_price from '@/components/table_4col_1_price'
-import React from 'react'
 
 const MainMenu = ({ menus }) => {
+  const items = [
+    //prettier-ignore
+    { name: 'starters', display: 'Starters'},
+    { name: 'soups', display: 'Soups' },
+    { name: 'chicken_duck', display: 'Chicken and Duck' },
+    { name: 'pork', display: 'Pork' },
+    { name: 'pork_ribs', display: 'Pork Ribs' },
+    { name: 'beef', display: 'Beef' },
+    { name: 'seafood', display: 'Seafood' },
+    { name: 'curry', display: 'Curry' },
+    { name: 'salad', display: 'Salad' },
+    { name: 'vegetables', display: 'Vegetables' },
+    { name: 'rice', display: 'Rice' },
+    { name: 'noodles', display: 'Rice' },
+  ]
   return (
     <>
-      <div className="bg-slate-100">
-        <Table4col_1_price menus={menus} title={'Main Menus'} />
+      <div className="bg-slate-100 menusBox">
+        {items.map(item => (
+          <Table4col_1_price
+            menus={menus.filter(m => m.category === item.name)}
+            title={item.display}
+          />
+        ))}
       </div>
     </>
   )
