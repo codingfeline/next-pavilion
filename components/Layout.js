816368f537205af2787e05197dc97356dd59339d
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-06 07:06:57 +0100

    conditional bg

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,20 @@
+import { useRouter } from 'next/router'
 import Footer from './Footer'
 import Navbar from './Navbar'
+import Image from 'next/image'
 
 const Layout = ({ children }) => {
+  const path = useRouter().pathname
   return (
     <>
-      <div className="bg-purple-200 flex flex-col min-h-screen justify-between">
+      {' '}
+      {path}
+      <div
+        className={`bg-purple-200 flex flex-col min-h-screen justify-between
+        ${path === '/' && 'bg_hom'}
+      `}
+      >
+        <Image src="/veg1.jpg" fill={true} objectFit={'cover'} />
         <Navbar />
         <div className="flex flex-col mb-auto p-4">
           <main>{children}</main>
