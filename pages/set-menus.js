3bc1e309d7fd4182f8d6d7a7240ff537a411adfd
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-06 23:43:42 +0100

    functional set menu, only 1 group so far

--- pages/set-menus.js
+++ pages/set-menus.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import Table4col_2_price from '@/components/table_4col_2_price'
 
-const SetMenus = () => {
+const SetMenus = ({ menus }) => {
+  const items = [
+    //prettier-ignore
+    { name: 'lunch_menus', title: 'Lunch Menus'},
+    { name: 'choices', title: 'Choices' },
+    { name: 'set_menu', title: 'Set Menus' },
+  ]
+
   return (
     <>
-      <div>SetMenus</div>
+      <div className="menusBox">
+        <Table4col_2_price
+          menus={menus.filter(m => m.category === 'lunch_menus')}
+          title="Lunch Menus"
+        />
+      </div>
     </>
   )
 }
 
 export default SetMenus
+
+export async function getStaticProps() {
+  const res = await fetch(
+    'https://bangkokpavilion.co.uk/api/all_menus?menu=set_and_lunch'
+  )
+  const menus = await res.json()
+
+  return {
+    props: { menus },
+  }
+}
