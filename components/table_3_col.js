df07062b2b381db23b00b610d8504108ed54f126
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-07 12:49:41 +0100

    drinks, conditional prices

---
+++ components/table_3_col.js
@@ -0,0 +1,45 @@
+import parse from 'html-react-parser'
+
+const Table3_col = ({ menus, title }) => {
+  return (
+    <>
+      <h3>{title}</h3>
+      <table className="menus table-auto">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Item</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {menus.map(menu => (
+            <tr key={menu.id}>
+              <td className="row_number">{menu.id}</td>
+              <td>{menu.item}</td>
+              <td>
+                {parse(menu.description)}
+                <span className="winePrices">
+                  {menu.price1 && menu.price2 && menu.price3 ? (
+                    <>
+                      <i>175ml: &pound; {menu.price1.toFixed(2)}</i>
+                      <i>250ml: &pound; {menu.price2.toFixed(2)}</i>
+                      <i>bottle: &pound; {menu.price3.toFixed(2)}</i>
+                    </>
+                  ) : (
+                    ''
+                  )}
+                  {!menu.price1 && !menu.price2 && menu.price3 && (
+                    <i>bottle: &pound; {menu.price3.toFixed(2)}</i>
+                  )}
+                </span>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default Table3_col
--- pages/drinks.js
+++ pages/drinks.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import Table3_col from '@/components/table_3_col'
 
-const Drinks = () => {
+const Drinks = ({ menus }) => {
   return (
-    <>
-      <div>Drinks</div>
-    </>
+    <div className="menusBox">
+      <Table3_col
+        menus={menus.filter(m => m.category === 'wine')}
+        title="Wines"
+      />
+    </div>
   )
 }
 
 export default Drinks
+
+export async function getStaticProps() {
+  const res = await fetch(
+    'https://bangkokpavilion.co.uk/api/all_menus?menu=drinks'
+  )
+  const menus = await res.json()
+
+  return {
+    props: { menus },
+  }
+}
