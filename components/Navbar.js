827aa34626f437e1d5b338c5b9a69e0a6ffe1b4d
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-07 17:12:22 +0100

    navbar active class

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,12 @@
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { useState, useEffect } from 'react'
 
 const Navbar = () => {
   const [hidden, setHidden] = useState(true)
   const toggleMenu = () => setHidden(!hidden)
   const collapse = () => setHidden(true)
+  const pathname = usePathname()
 
   useEffect(() => {
     window.addEventListener('scroll', collapse)
@@ -33,13 +35,26 @@ const Navbar = () => {
           onClick={collapse}
         >
           <ul className="flex flex-col justify-center items-center md:flex-row md:justify-around  bg-indigo-200 w-full h-full">
-            {links.map(link => (
-              <li key={link.to} className="nav-li">
-                <Link onClick={collapse} href={link.to} className="nav-a">
-                  {link.page.toUpperCase()}
-                </Link>
-              </li>
-            ))}
+            {links.map(link => {
+              const isActive = pathname === link.to
+              return (
+                <li key={link.to} className="nav-li">
+                  <Link
+                    onClick={collapse}
+                    href={link.to}
+                    className={`nav-a 
+                    ${
+                      isActive
+                        ? 'bg-slate-200 hover:bg-slate-100'
+                        : 'hover:bg-blue-300'
+                    }
+                  `}
+                  >
+                    {link.page.toUpperCase()}
+                  </Link>
+                </li>
+              )
+            })}
           </ul>
         </div>
       </nav>
