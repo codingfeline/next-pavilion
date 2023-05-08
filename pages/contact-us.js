785a5b6e0823d448b63312e47629d6557c5cc2d5
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-08 13:16:30 +0100

    useForm hook

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,34 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+// import { DevTool } from '@hookform/devtools'
 
 const ContactUs = () => {
+  const [info, setInfo] = useState('')
+  const { register, handleSubmit, control, formState } = useForm()
+  const onSubmit = data => setInfo(data)
+  const { errors } = formState
+
   return (
-    <>
+    <div className="bg-slate-100 p-5 rounded-lg">
       <h4>ContactUs</h4>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="name">
             Name
-            <input type="text" id="name" />
+            <span>{errors.name?.message}</span>
+            <input
+              type="text"
+              id="name"
+              {...register('name', { required: 'name is required' })}
+            />
           </label>
         </div>
         <div>
           <label htmlFor="email">
             Email
-            <input type="text" id="email" />
+            <span>{errors.email?.message}</span>
+            <input
+              type="text"
+              id="email"
+              {...register('email', {
+                required: 'email is required',
+                pattern: {
+                  value:
+                    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                  message: 'invalid email format',
+                },
+              })}
+            />
           </label>
         </div>
         <div>
           <label htmlFor="message">
             Message
-            <textarea id="message" cols="30" rows="10"></textarea>
+            <span>{errors.message?.message}</span>
+            <textarea
+              id="message"
+              cols="30"
+              rows="10"
+              {...register('message', { required: 'Message is required' })}
+            ></textarea>
           </label>
         </div>
         <div className="flex gap-4">
-          <button>Send</button>
+          <input type="submit" value="Submit" className="btn" />
           <button>Clear</button>
         </div>
       </form>
-    </>
+      {/* <DevTool control={control} /> */}
+    </div>
   )
 }
 
