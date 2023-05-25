0c18c47abbc716dc05e6cd08766cd13b1f6b2c8f
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-25 05:36:19 +0100

    refactored home - booking

---
+++ components/booking.jsx
@@ -0,0 +1,13 @@
+const Booking = () => {
+  return (
+    <div align="center" classname="widgetContainer">
+      <iframe
+        src="https://svtables.com/widget/?rest_id=403"
+        className="w-full rounded-xl mt-2"
+        height="680px"
+      ></iframe>
+    </div>
+  )
+}
+
+export default Booking
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 // import { Inter } from 'next/font/google'
+import Booking from '@/components/booking'
 import Head from 'next/head'
 
 // const inter = Inter({ subsets: ['latin'] })
@@ -13,25 +14,21 @@ export default function Home({ resources }) {
           Thailand, the land of smiles and its unique cuisine is the Buddhist
           land in South East Asia, the country which is formerly known as Siam
           has been renamed Thailand since 1939 in the era of King Rama VIII.
-          Thailand means
-        </p>
-        <p>
-          the land of freedom. The Thais are proud of their country, which has
-          never been a colony of any country. The capital city of Thailand is
-          Bangkok.
+          Thailand means the land of freedom.
         </p>
         DINE-IN OR TAKE AWAY 01304 367 707 (click/tap to phone) 114 HIGH STREET
-        CT14 6BB, DEAL, KENT, UK 20% discount on all take-aways 17:30-23:00
-        Tuesdays to Sundays Closed on Mondays
+        CT14 6BB, DEAL, KENT, UK
+        <div>20% discount on all take-aways</div>
+        <aside itemscope itemtype="http://schema.org/LocalBusiness">
+          {/* <time itemprop="openingHours" datetime="Tu-Su 17:30-23:00" */}
+          <span itemprop="openingHours" content="Tu,We,Th,Fr,Sa,Su 17:30-23:00">
+            17:30-23:00 Tuesdays to Sundays
+          </span>
+          <div>Closed on Mondays</div>
+        </aside>
         {/* style="width:100%;height:700px" */}
       </div>
-      <div align="center" classname="widgetContainer">
-        <iframe
-          src="https://svtables.com/widget/?rest_id=403"
-          className="w-full rounded-xl mt-2"
-          height="680px"
-        ></iframe>
-      </div>
+      <Booking />
     </>
   )
 }
