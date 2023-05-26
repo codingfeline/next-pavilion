a17fa72d3a4b2ead482383e83ab3ac5a2b836f5c
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-26 23:52:24 +0100

    antispam working

--- components/antispam.jsx
+++ components/antispam.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
+import { FaCheck, FaCross } from 'react-icons/fa'
 
-const Antispam = () => {
+const Antispam = ({ setPermission }) => {
   const [no1, setNo1] = useState('')
   const [no2, setNo2] = useState('')
   const [check1, setCheck1] = useState(false)
@@ -11,16 +12,20 @@ const Antispam = () => {
   const rand1 = () => setNo1(Math.floor(Math.random() * 10 + 1))
   const rand2 = () => setNo2(Math.floor(Math.random() * 10 + 1))
 
-  const compare1 = val => {
-    alert(val)
-    // if (val === no1) {
-    //   setCheck1(true)
-    // } else {
-    //   setCheck1(false)
-    // }
+  const randomize = () => {
+    rand1()
+    rand2()
+  }
+
+  const compare1 = a => {
+    if (a === no1) setCheck1(true)
+    else setCheck1(false)
   }
   const compare2 = val => {
-    if (val === no2) setCheck2(true)
+    if (val === no2 && check1) setCheck2(true)
+    setTimeout(() => {
+      setPermission(true)
+    }, 1500)
   }
 
   useEffect(() => {
@@ -31,31 +36,43 @@ const Antispam = () => {
   return (
     <>
       <h2>antispam</h2>
-      <h3>
-        <span className="inline-block w-1/6 bg-slate-400 m-1 p-1 text-center">
-          {no1}
-        </span>
-        {arr1.map(a => (
-          <span
-            className="bg-red-300 mx-1 p-1 cursor-pointer hover:bg-red-400"
-            onClick={compare1}
-          >
-            {a}
+      <div className={`bg-red-100 `}>
+        <h3>
+          <span className="inline-block w-1/6 bg-slate-400 m-1 p-1 text-center">
+            {no1}
           </span>
-        ))}{' '}
-        {check1 ? 'true' : 'false'}
-      </h3>
-      <h3>
-        <span className="inline-block w-1/6 bg-slate-400 m-1 p-1 text-center">
-          {no2}
-        </span>
-        {arr2.map(a => (
-          <span className="bg-red-300 mx-1 p-1 cursor-pointer hover:bg-red-400">
-            {a}
-          </span>
-        ))}
-      </h3>
-      <button onClick={rand1}>randomize</button>
+          {arr1.map(a => (
+            <span
+              className="bg-red-300 mx-1 p-1 cursor-pointer hover:bg-red-400"
+              onClick={() => compare1(a)}
+            >
+              {a}
+            </span>
+          ))}
+          {check1 ? (
+            <FaCheck className="text-green-500 inline-block ml-2" />
+          ) : (
+            ''
+          )}
+        </h3>
+        {check1 && (
+          <h3>
+            <span className="inline-block w-1/6 bg-slate-400 m-1 p-1 text-center">
+              {no2}
+            </span>
+            {arr2.map(a => (
+              <span
+                className="bg-red-300 mx-1 p-1 cursor-pointer hover:bg-red-400"
+                onClick={() => compare2(a)}
+              >
+                {a}
+              </span>
+            ))}
+            {check2 && <FaCheck className="text-green-500 inline-block ml-2" />}
+          </h3>
+        )}
+      </div>
+      {/* <button onClick={randomize}>randomize</button> */}
     </>
   )
 }
--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,3 +1,4 @@
+import Antispam from '@/components/antispam'
 import axios from 'axios'
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
@@ -8,6 +9,9 @@ const ContactUs = () => {
   const [error, setError] = useState('')
   const { register, handleSubmit, control, formState, reset } = useForm()
   const { errors } = formState
+  const [allowForm, setAllowForm] = useState(false)
+
+  const setPermission = flag => setAllowForm(flag)
 
   const onSubmit = data => {
     console.log(data)
@@ -32,58 +36,61 @@ const ContactUs = () => {
   return (
     <div className="bg-slate-100 p-5 rounded-lg">
       <h4>ContactUs</h4>
-      {submitted ? (
-        'submitted'
-      ) : (
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div>
-            <label htmlFor="name">
-              Name
-              <span>{errors.name?.message}</span>
-              <input
-                type="text"
-                id="name"
-                {...register('name', { required: 'name is required' })}
-              />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="email">
-              Email
-              <span>{errors.email?.message}</span>
-              <input
-                type="text"
-                id="email"
-                {...register('email', {
-                  required: 'email is required',
-                  pattern: {
-                    value:
-                      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                    message: 'invalid email format',
-                  },
-                })}
-              />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="message">
-              Message
-              <span>{errors.message?.message}</span>
-              <textarea
-                id="message"
-                cols="30"
-                rows="10"
-                {...register('message', { required: 'Message is required' })}
-              ></textarea>
-            </label>
-          </div>
-          <div className="flex gap-4">
-            <input type="submit" value="Submit" className="btn" />
-            <button onClick={resetForm}>Clear</button>
-          </div>
-        </form>
-      )}
+      {submitted
+        ? 'submitted'
+        : allowForm && (
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div>
+                <label htmlFor="name">
+                  Name
+                  <span>{errors.name?.message}</span>
+                  <input
+                    type="text"
+                    id="name"
+                    {...register('name', { required: 'name is required' })}
+                  />
+                </label>
+              </div>
+              <div>
+                <label htmlFor="email">
+                  Email
+                  <span>{errors.email?.message}</span>
+                  <input
+                    type="text"
+                    id="email"
+                    {...register('email', {
+                      required: 'email is required',
+                      pattern: {
+                        value:
+                          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                        message: 'invalid email format',
+                      },
+                    })}
+                  />
+                </label>
+              </div>
+              <div>
+                <label htmlFor="message">
+                  Message
+                  <span>{errors.message?.message}</span>
+                  <textarea
+                    id="message"
+                    cols="30"
+                    rows="10"
+                    {...register('message', {
+                      required: 'Message is required',
+                    })}
+                  ></textarea>
+                </label>
+              </div>
+              <div className="flex gap-4">
+                <input type="submit" value="Submit" className="btn" />
+                <button onClick={resetForm}>Clear</button>
+              </div>
+            </form>
+          )}
       {/* <DevTool control={control} /> */}
+      {!allowForm && <Antispam setPermission={setPermission} />}
     </div>
   )
 }
