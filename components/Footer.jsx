0e8f9564590f1a54299f9abf89b9001db19f92fe
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-29 14:37:29 +0100

    address and map

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import Link from 'next/link'
+import { FaMap } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <footer className="bg-slate-100 py-2 px-4 z-10">
-      &copy;{` ${new Date().getFullYear()}`}
+    <footer
+      className="bg-slate-100 py-2 px-4 z-10 flex flex-col items-center sm:flex-row
+    justify-between"
+    >
+      <span>&copy;{` ${new Date().getFullYear()}`} Bangkok Pavilion</span>
+      <Link href="/locate-us" className="flex gap-2">
+        <FaMap className="text-lg " />
+        <address>114 HIGH STREET CT14 6BB, DEAL, KENT, UK</address>
+      </Link>
     </footer>
   )
 }
