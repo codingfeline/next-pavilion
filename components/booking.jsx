0f2e21a1daa5533d809c7601acf04fd5a4dc0a7f
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-29 14:38:33 +0100

    responsive width

--- components/booking.jsx
+++ components/booking.jsx
@@ -1,9 +1,9 @@
 const Booking = () => {
   return (
-    <div align="center" classname="widgetContainer">
+    <div align="center" classname="widgetContainer ">
       <iframe
         src="https://svtables.com/widget/?rest_id=403"
-        className="w-full rounded-xl mt-2"
+        className="w-full sm:w-4/5 sm:rounded-xl mt-2"
         height="680px"
       ></iframe>
     </div>
