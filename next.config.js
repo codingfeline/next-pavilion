fd8944174df49dd2822cce1c3d25ab5a0653a3cf
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-06-21 22:18:53 +0100

    working export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
+
+// trailingSlash: true,
+// distDir: 'naz2',
+// basePath: '/dev',
